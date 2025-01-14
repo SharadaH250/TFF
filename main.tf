43b39aefa2ec2fabba598cf0ba4954c7097ca49c
Sharada H <hosallisharada.95@gmail.com>
2025-01-14 15:57:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,22 +3,22 @@ provider "aws" {
 }
 
 resource "aws_instance" "instance-1" {
-    ami = "ami-037774efca2da0726"
+    ami = "ami-05576a079321f21f8"
     instance_type = "t2.micro"
     count = "1"
     security_groups = ["default"]
-    key_name = "kiran"
+    key_name = "tf"
     tags = {
       Name = "grafan"
     }
 }
 
 resource "aws_instance" "instance-2" {
-    ami = "ami-037774efca2da0726"
+    ami = "ami-05576a079321f21f8"
     instance_type = "t2.micro"
     count = "1"
     security_groups = ["default"]
-    key_name = "kiran"
+    key_name = "tf"
     tags = {
       Name = "Node-port"
     }
